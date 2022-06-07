5235993046570e726d70ac8f7243b6441888e600
AnthonyR <arankin789@gmail.com>
2022-06-06 20:26:43 -0400

    finished Review model

--- models/Review.js
+++ models/Review.js
@@ -5,9 +5,42 @@ const User = require('./User')
 
 // Create the Review Model
 
-class Review extends Model {}
+class Review extends Model {
+    // add in vote logic
+}
 
 // use sequelize to create fields/columns
-Review.init({})
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        // Rating field | User can rate the artist/song/album on a scale of 1-10
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'review'
+    }
+)
 
 module.exports = Review;
