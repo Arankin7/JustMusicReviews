13fff8dd994348d31b487493c22546244eb6c17f
AnthonyR <arankin789@gmail.com>
2022-06-08 20:50:02 -0400

    finished seeds|finished/fixed models

--- models/index.js
+++ models/index.js
@@ -4,3 +4,58 @@ const Comment = require('./Comment');
 const Vote = require('./Vote');
 
 // create associations between models
+
+User.hasMany(Review, {
+    foreignKey: 'user_id'
+});
+
+Review.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.belongsToMany(Review, {
+    through: Vote,
+    as: 'voted_review',
+    foreignKey: 'user_id'
+});
+
+Review.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_review',
+    foreignKey: 'review_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Review, {
+    foreignKey: 'review_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Review.hasMany(Vote, {
+    foreignKey: 'review_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Review, {
+    foreignKey: 'review_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Review.hasMany(Comment, {
+    foreignKey: 'review_id'
+});
+
+
+module.exports = { User, Review, Comment, Vote};
